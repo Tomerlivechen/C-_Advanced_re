169b55f64c528c5e01131e2aa9dae35ca2e5bf6c
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-06-01 22:57:41 +0300

    moving folders around

--- Animal_Facts/obj/Debug/net8.0-windows/API_hub.AssemblyInfo.cs
+++ Animal_Facts/obj/Debug/net8.0-windows/API_hub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_hub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f3682b9a0982a455c52cb760e290ed39f775b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7196c4c24a6abf90ef36422c0ccc3bb43a77db46")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_hub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_hub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
